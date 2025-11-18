37f0b83abda0daa9894b53e37fa6495f44376240
rishika301 <rishikan603@gmail.com>
2025-11-18 12:31:10 +0530

    Add multiplication table program in tables.c

---
+++ tables.c
@@ -0,0 +1,14 @@
+
+   // tables
+#include <stdio.h>
+int main() 
+{
+   int num,i;
+   printf("enter the value of num");
+   scanf("%d",&num);
+   
+   for(i=1;i<=10; i++)
+   printf("%d*%d=%d\n",num,i,num*i);
+}
+   
+   
