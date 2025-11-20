bc3b0fd31fbc2f811d5aaa9448ce6a852d94044a
rishika301 <rishikan603@gmail.com>
2025-11-20 11:13:24 +0530

    Add palindrome checking functionality
    Implement palindrome check for an integer input

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,rev=0,ori,rem;
+    printf("enter number");
+    scanf("%d",&num);
+    ori=num;
+    while(num!=0){
+    rem=num%10;
+    rev=rev*10+rem;
+    num=num/10;}
+    if(ori==rev){
+    printf("palindrome");}
+    else
+   { printf("it is not palindrome");}
+}
